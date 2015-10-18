a6c4f346383dda7065ba6af02a45a27e0e38c65b
Caleb Evans <caleb@calebevans.me>
2015-10-18 14:25:15 -0700

    Do not initialize plugin if already initialized

--- awesome-cpt.php
+++ awesome-cpt.php
@@ -8,17 +8,16 @@
  * License:     MIT
  */
 
-// Define path to AwesomeCPT directory
-if ( ! defined( 'AWESOME_CPT_DIR' ) ) {
-	define( 'AWESOME_CPT_DIR', dirname( __FILE__ ) );
-}
-// Define constant indicating the existence of AwesomeCPT
+// The AWESOME_CPT constant indicates whether Awesome CPT has been loaded
 if ( ! defined( 'AWESOME_CPT' ) ) {
-	define( 'AWESOME_CPT', true );
-}
 
-// Import AwesomeCPT classes
-require_once AWESOME_CPT_DIR . '/classes/class-awesome-base-type.php';
-require_once AWESOME_CPT_DIR . '/classes/class-awesome-post-type.php';
-require_once AWESOME_CPT_DIR . '/classes/class-awesome-taxonomy.php';
-require_once AWESOME_CPT_DIR . '/classes/class-awesome-meta-box.php';
+	define( 'AWESOME_CPT_DIR', dirname( __FILE__ ) );
+	define( 'AWESOME_CPT', true );
+
+	// Import AwesomeCPT classes
+	require_once AWESOME_CPT_DIR . '/classes/class-awesome-base-type.php';
+	require_once AWESOME_CPT_DIR . '/classes/class-awesome-post-type.php';
+	require_once AWESOME_CPT_DIR . '/classes/class-awesome-taxonomy.php';
+	require_once AWESOME_CPT_DIR . '/classes/class-awesome-meta-box.php';
+
+}
