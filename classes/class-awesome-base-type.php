5741695196e2ccc2ca8f51d5b1de304c0674914f
Caleb Evans <caleb@calebevans.me>
2015-11-26 21:37:32 -0800

    Add contextual help feature

--- classes/class-awesome-base-type.php
+++ classes/class-awesome-base-type.php
@@ -27,20 +27,39 @@ abstract class Awesome_Base_Type {
 		}
 
 		// If contextual help function is given
-		if ( ! empty( $this->contextual_help ) ) {
-			// Add contextual help to type
-			add_action( 'contextual_help', array( $this, 'contextual_help' ), 10, 3 );
+		if ( ! empty( $this->help ) ) {
+			$this->add_all_help_menus();
 		}
 
 	}
 
-	// Adds contextual help for type
-	public function contextual_help( $contextual_help, $screen_id, $screen ) {
+	// Adds contextual help menus to all specified screens
+	public function add_all_help_menus() {
 
-		// If contextual help callback was given
-		if ( $this->contextual_help ) {
-			// Call function
-			call_user_func_array( $this->contextual_help, array( $contextual_help, $screen_id, $screen ) );
+		foreach ( $this->help as $screen_id => $help_menu ) {
+			add_action( "admin_head", array( $this, 'add_help_menu', ) );
+		}
+
+	}
+
+	// Adds a single contextual help menu to the current screen
+	public function add_help_menu() {
+
+		$screen = get_current_screen();
+
+		if ( ! empty( $this->help[ $screen->id ] ) ) {
+			$help_menu = $this->help[ $screen->id ];
+
+			// Add the given help tabs
+			foreach ( $help_menu['tabs'] as $tab ) {
+				$screen->add_help_tab( $tab );
+			}
+
+			// Add right-hand help sidebar if given
+			if ( ! empty( $help_menu['sidebar'] ) ) {
+				$screen->set_help_sidebar( $help_menu['sidebar'] );
+			}
+
 		}
 
 	}
