b862fb8b9a43ee2a29ecc7d6871330892a7ed4b9
Caleb Evans <caleb@calebevans.me>
2015-11-26 17:53:35 -0800

    Fix whitespace and revise comments for meta box class

--- classes/class-awesome-meta-box.php
+++ classes/class-awesome-meta-box.php
@@ -10,41 +10,49 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 		'after'  => '</p>'
 	);
 
+	// Map each essential field type to a class function that constructs it
 	static $field_callbacks = array(
 		'select'   => 'populate_select',
 		'textarea' => 'populate_textarea',
 		'select'   => 'populate_select',
 		'image'    => 'populate_image',
 		'checkbox' => 'populate_checkbox',
-		'radio'    => 'populate_checkbox'
+		'radio'    => 'populate_checkbox',
+		'text'     => 'populate_input',
+		'default'  => 'populate_input'
 	);
 
+	// Standard attributes available to any form element
 	static $std_attrs = array( 'id', 'name', 'class', 'placeholder', 'pattern' );
 
-	// Meta Box constructor
 	function __construct( $params ) {
+
 		$this->merge_params( $params );
 		$this->nonce_id = "{$this->id}-nonce";
-		// Post types array defaults to empty array
+
+		// Initialize array-type properties if they're not supplied
 		if ( empty( $this->post_types ) ) {
 			$this->post_types = array();
 		}
-		// Fields array defaults to empty array
 		if ( empty( $this->fields ) ) {
 			$this->fields = array();
 		}
-		// Callback arguments array defaults to empty array
 		if ( empty( $this->callback_args ) ) {
 			$this->callback_args = array();
 		}
+
+		// Merge field defaults into every field
 		$this->modify_fields();
-		// Add actions to make meta boxes functional
+
+		// Bind actions to make meta boxes functional
 		add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ), 10 );
 		add_action( 'save_post', array( $this, 'save_post' ), 10 );
+
 	}
 
-	// Modify the fields for this meta box
+	// Modifies the given meta box fields by supplying defaults
 	public function modify_fields() {
+
 		foreach ( $this->fields as &$field ) {
 			// Field name defaults to field ID
 			if ( empty( $field['name'] ) ) {
@@ -55,10 +63,12 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 			// Add field to list of all fields with same name
 			$this->field_names[ $field['name'] ][] = $field;
 		}
+
 	}
 
-	// Action callback for adding meta box for all given post types
+	// Register this meta box for all assigned post types
 	public function add_meta_boxes() {
+
 		foreach ( $this->post_types as $post_type_id ) {
 			add_meta_box(
 				$this->id,
@@ -70,32 +80,41 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 				$this->callback_args
 			);
 		}
+
 	}
 
-	// Output the escaped HTML value
+	// Outputs the escaped HTML value (between optional before/after text)
 	public function echo_value( $before, $value, $after ) {
+
 		echo $before, esc_html( $value ), $after;
+
 	}
 
-	// Retrieve and output the given HTML attributes
+	// Retrieves, escapes, and outputs the given HTML attributes
 	public function echo_attrs( $field, $attrs ) {
+
 		foreach ( $attrs as $attr ) {
 			if ( isset( $field[ $attr ] ) ) {
 				$this->echo_value( " {$attr}='", $field[ $attr ], "'" );
 			}
 		}
+
 	}
 
-	// Echo the attributes standard to all field types
+	// Outputs the attributes standard to all field types
 	public function echo_std_attrs( $meta_value, $field, $post ) {
+
 		$this->echo_attrs( $field, self::$std_attrs );
+		// The 'required' attribute is special because it has no value
 		if ( ! empty( $field['required'] ) ) {
 			echo " required";
 		}
+
 	}
 
-	// Indicate if the given field is selected/checked or not
+	// Returns a boolean indicating if the given field is selected/checked
 	public function is_field_selected( $meta_value, $field, $attr ) {
+
 		return (
 			(
 				$meta_value && ! empty( $field['value'] ) &&
@@ -106,11 +125,14 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 						in_array( $field['value'], $meta_value )
 					)
 				)
-			) || ( ! $meta_value && ! empty( $field[ $attr ] ) ) );
+			) || ( ! $meta_value && ! empty( $field[ $attr ] ) )
+		);
+
 	}
 
-	// Populate a checkbox/radio field
+	// Outputs the HTML for a checkbox/radio field
 	public function populate_checkbox( $meta_value, $field, $post ) {
+
 		// If this is the first field with this particular name
 		if ( $this->field_names[ $field['name'] ][0] === $field ) {
 			// Create hidden input to make unchecked checkbox visible to PHP
@@ -120,7 +142,8 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 				'type'  => 'hidden'
 			) ), $post );
 		}
-		// Output checkbox
+
+		// Output checkbox element
 		echo "<input";
 		$this->echo_std_attrs( $meta_value, $field, $post );
 		$this->echo_attrs( $field, array( 'type', 'value' ) );
@@ -130,19 +153,23 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 			echo " checked";
 		}
 		echo " />";
+
 	}
 
-	// Populate an image input
+	// Outputs the HTML for an image input
 	public function populate_image( $meta_value, $field, $post ) {
+
 		echo "<input";
 		$this->echo_std_attrs( $meta_value, $field, $post );
 		$this->echo_attrs( $field, array( 'type', 'value', 'src', 'alt' ) );
 		echo " />";
+
 	}
 
-	// Populate a select (dropdown) menu
+	// Outputs the HTML for a select (dropdown) menu
 	public function populate_select( $meta_value, $field, $post ) {
-		// Output containing select element
+
+		// Output select element
 		echo "<select";
 		$this->echo_std_attrs( $meta_value, $field, $post );
 		// If select field allows for multiple values
@@ -151,121 +178,140 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 			echo " multiple";
 		}
 		echo ">";
+
 		// If value for options field is given
 		if ( ! empty( $field['options'] ) ) {
 			if ( is_callable( $field['options'] ) ) {
-				// Accept options array returned from the given callback
+				// Accept options array returned from the given callback (if
+				// 'options' is a callback)
 				$options = call_user_func_array( $field['options'], array( $meta_value, $field, $post ) );
 			} elseif ( is_array( $field['options'] ) ) {
-				// Otherwise, use options array if given
+				// Otherwise, assume 'options' in an array
 				$options = $field['options'];
 			}
 		} else {
 			// The options array is empty by default
 			$options = array();
 		}
+
 		// Output each option element
 		foreach ( $options as $option ) {
 			echo "<option";
 			$this->echo_attrs( $option, array( 'value', 'class' ) );
-			// If option should be selected
+			// If option should be selected, add attribute
 			if ( $this->is_field_selected( $meta_value, $option, 'selected' ) ) {
-				// Mark option as selected
 				echo " selected";
 			}
-			// Output option label
+			// Output content of option element
 			$this->echo_value( ">", $option['content'], "</option>" );
 		}
+
 		echo "</select>";
+
 	}
 
 
-	// Populate a textarea
+	// Outputs the HTML for a multiline textarea element
 	public function populate_textarea( $meta_value, $field, $post ) {
+
 		echo "<textarea";
 		$this->echo_std_attrs( $meta_value, $field, $post );
 		$this->echo_attrs( $field, array( 'rows', 'cols' ) );
-		// If meta value is not set for this post
+
+		// If meta value is not set for this post, output default value
 		if ( ! empty( $field['content'] ) && '' === $meta_value ) {
-			// Output default value
 			$this->echo_value( ">", $field['content'], "</textarea>" );
 		} else {
 			// Otherwise, output retrieved meta value
 			$this->echo_value( ">", $meta_value, "</textarea>" );
 		}
+
 	}
 
-	// Populate a custom field created by callback function
+	// Outputs the HTML for a custom field determined by the given callback
 	public function populate_custom( $meta_value, $field, $post ) {
+
 		if ( ! empty( $field['populate'] ) ) {
 			call_user_func_array( $field['populate'], array( $meta_value, $field, $post ) );
 		}
+
 	}
 
-	// Populate a regular input field (text, email, etc.)
+	// Outputs the HTML for a regular input field (text, email, etc.)
 	public function populate_input( $meta_value, $field, $post ) {
+
 		// Output input element
 		echo "<input";
 		$this->echo_std_attrs( $meta_value, $field, $post );
 		$this->echo_attrs( $field, array( 'type', 'min', 'max', 'step' ) );
-		// If meta value is not set for this post
+
+		// If meta value is not set for this post, output default value
 		if ( ! empty( $field['value'] ) && '' === $meta_value ) {
-			// Output default value
 			$this->echo_value( " value='", $field['value'], "'" );
 		} else {
 			// Otherwise, output retrieved meta value
 			$this->echo_value( " value='", $meta_value, "'" );
 		}
 		echo " />";
+
 	}
 
-	// Populate a field label
+	// Outputs the HTML for a field label
 	public function populate_label( $meta_value, $field, $post ) {
-		// Output field label if given
+
 		if ( ! empty( $field['label'] ) ) {
 			echo "<label";
 			$this->echo_value( " for='", $field['id'], "'" );
 			$this->echo_value( ">", $field['label'], "</label>" );
 		}
+
 	}
 
-	// Populate the given field for the given post
+	// Outputs the HTML for the given field for the given post
 	public function populate_field( $meta_value, $field, $post ) {
+
 		// If filter callback is given
 		if ( ! empty( $field['display'] ) ) {
 			// Filter displayed meta value
 			$meta_value = call_user_func_array( $field['display'], array( $meta_value, $field, $post ) );
 		}
-		// If custom callback reference is given
+
 		if ( ! empty( $field['populate'] ) ) {
-			// Output field using callback
+
+			// If custom callback reference is given, use it to output HTML
 			$this->populate_custom( $meta_value, $field, $post );
+
 		} else {
-			// Output HTML to appear before field
+			// Otherwise, output HTML based on supplied parameters
+
+			// Output any custom HTML to appear before field
 			echo $field['before'];
-			// Handle all supported input types
 			if ( 'checkbox' === $field['type'] || 'radio' === $field['type'] ) {
-				$this->populate_checkbox( $meta_value, $field, $post );
+				// Label should appear after checkboxes and radio buttons
+
+				call_user_func_array( array( $this, self::$field_callbacks[ $field['type'] ] ), array( $meta_value, $field, $post ) );
 				$this->populate_label( $meta_value, $field, $post );
+
 			} else {
+				// For all other field types, label should precede input
+
 				$this->populate_label( $meta_value, $field, $post );
-				if ( 'image' === $field['type'] ) {
-					$this->populate_image( $meta_value, $field, $post );
-				} elseif ( 'select' === $field['type'] ) {
-					$this->populate_select( $meta_value, $field, $post );
-				} elseif ( 'textarea' === $field['type'] ) {
-					$this->populate_textarea( $meta_value, $field, $post );
+				if ( ! empty( $field['type'] ) ) {
+					call_user_func_array( array( $this, self::$field_callbacks[ $field['type'] ] ), array( $meta_value, $field, $post ) );
 				} else {
-					$this->populate_input( $meta_value, $field, $post );
+					call_user_func_array( array( $this, self::$field_callbacks[ 'default' ] ), array( $meta_value, $field, $post ) );
 				}
+
 			}
-			// Output HTML to appear after field
+			// Output any custom HTML to appear after field
 			echo $field['after'];
+
 		}
 	}
 
-	// Populate meta box with the defined fields
+	// Outputs HTML for all fields for this meta box
 	public function populate_fields( $post ) {
+
 		// Add nonce field for security
 		wp_nonce_field( plugin_basename( __FILE__ ), $this->nonce_id );
 		// Output each field
@@ -275,15 +321,19 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 			// Populate the given field with the given value
 			$this->populate_field( $meta_value, $field, $post );
 		}
+
 	}
 
-	// Get POST variable from given name (supports brackets)
+	// Retrieves POST variable from given name (supports nesting with [])
 	public function get_post_var( $name ) {
-		$key_matches = array();
+
 		// Retrieve key names from the given name attribute
+		$key_matches = array();
 		preg_match_all( '/[^\[\]]+/', $name, $key_matches );
+
 		// If there is at least one key in name
 		if ( count( $key_matches ) !== 0 ) {
+
 			// Retrieve list of key names from matches
 			$key_names = $key_matches[0];
 			// Search for POST value based on hierarchy
@@ -295,21 +345,26 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 					$val = null;
 				}
 			}
+
 		}
+
 		return $val;
 	}
 
-	// Save meta data on post save
+	// Saves meta data on post save
 	public function save_post( $post_id ) {
 		global $post;
+
 		// Do not save if autosave is in progress
 		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
 			return;
 		}
+
 		// Do not save if nonce field does not validate
 		if ( ! empty( $_POST[ $this->nonce_id ] ) && ! wp_verify_nonce( $_POST[ $this->nonce_id ], plugin_basename( __FILE__ ) ) ) {
 			return;
 		}
+
 		// Do not save if unprivileged user edits post
 		if ( ! empty( $_POST['post_type'] ) && 'page' === $_POST['post_type'] ) {
 			if ( ! current_user_can( 'edit_page', $post_id ) ) {
@@ -320,26 +375,34 @@ class Awesome_Meta_Box extends Awesome_Base_Type {
 				return;
 			}
 		}
+
 		// Loop through all associated post types
 		foreach ( $this->post_types as $post_type_id ) {
 			// If current post type is an associated post type
 			if ( ! empty( $_POST['post_type'] ) && $_POST['post_type'] === $post_type_id ) {
 				// Save each field to database
 				foreach ( $this->fields as $field ) {
+
 					$meta_value = $this->get_post_var( $field['name'] );
 					if ( null !== $meta_value ) {
+
 						// If filter callback is given
 						if ( ! empty( $field['save'] ) ) {
 							// Filter saved meta value
 							$meta_value = call_user_func_array( $field['save'], array( $meta_value, $field, $post ) );
 						}
+
 						// If value has changed
 						if ( $meta_value !== get_post_meta( $post_id, $field['name'], true ) ) {
 							// Update database value for field
 							update_post_meta( $post_id, $field['name'], $meta_value );
 						}
+
 					}
+
 				}
+				// Stop as soon as this meta box is found to support the page's
+				// current post type
 				break;
 			}
 		}
