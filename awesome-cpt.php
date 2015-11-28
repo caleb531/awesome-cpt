00a7c8e21b01bb9846aa9959960ecd4378b54dde
Caleb Evans <caleb@calebevans.me>
2015-11-28 14:07:46 -0800

    Prepare v1.1.0 release

--- awesome-cpt.php
+++ awesome-cpt.php
@@ -4,7 +4,7 @@
  * Description: The easiest and most flexible way to code custom post types in WordPress
  * Author:      Caleb Evans
  * Author URI:  http://calebevans.me/
- * Version:     1.0.2
+ * Version:     1.1.0
  * License: GNU General Public License v2.0
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
